87aded3dccf713b30aaa8b6d3bf593a30002d9dd
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-04-02 15:57:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your JS code here. If required.
-let 
+let checkbox=document.getElementById("checkbox")
+// checkbox.addEventListener("click",(e)=>{
+// 	if(e.target.checked){
+// 		alert("HEYUCHECKE")
+// 	}
+// })
+let submitbtn=document.getElementById("submit");
+let userName=document.getElementById("username");
+let password=document.getElementById("password")
+submitbtn.addEventListener("click",()=>{
+	let obj={
+	
+	usetex:userName.value,
+	pas:password.value,
+	}
+	
+	alert(`Logged in as ${obj.usetex}`)
+	
+	if(checkbox.checked)
+	{
+	sessionStorage.setItem('checkboxState',JSON.stringify(obj));
+	}
+	else{
+		sessionStorage.removeItem('checkboxState');
+	}
+})
